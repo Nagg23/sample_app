1a1a4903a779378f7b35898ff36865ae2c05ea28
Nagg23 <rubylearner.hatena@gmail.com>
2015-02-08 14:13:05 +0000

    Finish static pages & RSpec TDD of Rails Tutorial

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,7 +38,7 @@ describe "Static Pages" do
 
 	describe "About page" do
 	    it "should have the title 'About Us'" do
-	    	visit '/static_pages/About'
+	    	visit '/static_pages/about'
 	    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 	    end
 	end
